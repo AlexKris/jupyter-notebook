9aa0086a5b9fb463a25e82280b6baf284224b5eb
gaoruiting <oi.yi@outlook.com>
2023-02-07 17:52:03 +0800

    第12章 外星人游戏

---
+++ python_programming_from_entry_to_practice/project_game/ship.py
@@ -0,0 +1,33 @@
+import pygame
+
+class Ship:
+    """管理飞船的类"""
+
+    def __init__(self, ai_game):
+        """初始化飞船并设置其出示位置"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
+        # 加载飞船图像并获取其外接矩形
+        # pygame.image.load()加载图片，法牛表示飞船的surface
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # 对于每艘新飞船，都将其放在屏幕底部的中央
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """根据移动标志调整飞船位置，修改self.x前检查飞船位置，self.rect.right飞船矩形右边缘，self.screen_rect.right屏幕右边缘"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
+
+    def blitme(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image, self.rect)
